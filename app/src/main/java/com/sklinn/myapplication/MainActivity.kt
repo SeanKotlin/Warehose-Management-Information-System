acb5572405b2bc07f90b9b352a592efa5830fe32
sklinn <soe.khaing.linn86@gmail.com>
2021-06-16 00:53:47 +0630

    drawer layout, toolbar, menu_click

--- app/src/main/java/com/sklinn/myapplication/MainActivity.kt
+++ app/src/main/java/com/sklinn/myapplication/MainActivity.kt
@@ -2,10 +2,41 @@ package com.sklinn.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var listener: NavController.OnDestinationChangedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        nav_view.setupWithNavController(navController)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navController
+        return navController.navigateUp(appBarConfiguration)||super.onSupportNavigateUp()
+    }
+
 }
