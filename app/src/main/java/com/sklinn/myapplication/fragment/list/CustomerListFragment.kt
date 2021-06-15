494712955d08e581a9f02e5cab5efe6317f5a59e
sklinn <soe.khaing.linn86@gmail.com>
2021-06-16 00:00:14 +0630

    all fragment and drawer layout menu

---
+++ app/src/main/java/com/sklinn/myapplication/fragment/list/CustomerListFragment.kt
@@ -0,0 +1,21 @@
+package com.sklinn.myapplication.fragment.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sklinn.myapplication.R
+
+class CustomerListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_customer, container, false)
+    }
+
+
+}
